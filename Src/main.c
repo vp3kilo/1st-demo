ef2541160872fce80758fff624d244348039035e
VINHPHAM\kilolt <vp3kilo@gmail.com>
2018-06-29 17:49:13 +0700

    trình bày lại file và đổi tên file readme

--- Src/main.c
+++ Src/main.c
@@ -2,48 +2,42 @@
 
 
 /*************************************************************************************************/
-void Reserved_IRQHandler(void)
-{
+void Reserved_IRQHandler(void){
   while(1)
   {
     /* nothing to be run here */
   }
 }
 
-void NMI_Handler(void)
-{
+void NMI_Handler(void){
   while(1)
   {
     /* nothing to be run here */
   }
 }
 
-void HardFault_Handler(void)
-{
+void HardFault_Handler(void){
   while(1)
   {
     /* nothing to be run here */
   }
 }
 
-void SVC_Handler(void)
-{
+void SVC_Handler(void){
   while(1)
   {
     /* nothing to be run here */
   }
 }
 
-void PendSV_Handler(void)
-{
+void PendSV_Handler(void){
   while(1)
   {
     /* nothing to be run here */
   }
 }
 
-void SysTick_Handler(void)
-{
+void SysTick_Handler(void){
   while(1)
   {
     /* nothing to be run here */
@@ -51,8 +45,7 @@ void SysTick_Handler(void)
 }
 /*************************************************************************************************/
 
-void delay(unsigned int timeout)
-{
+void delay(unsigned int timeout){
     unsigned int t1, t2;
     for (t1 = timeout; t1 > 0; --t1)
     {
@@ -63,8 +56,7 @@ void delay(unsigned int timeout)
     }
 }
 
-void enabled_clock(void)
-{
+void enabled_clock(void){
   /* Enable clock PORT C*/
 	write_reg(RCC_AHBENR, read_reg(RCC_AHBENR, ~(1 << 19)) | (1 << 19));
 
@@ -72,8 +64,7 @@ void enabled_clock(void)
   write_reg(RCC_AHBENR, read_reg(RCC_AHBENR, ~(1 << 17)) | (1 << 17));
 }
 
-void init_pin(void)
-{
+void init_pin(void){
   /* Init PC9 ~ OUTPUT*/
 	write_reg(GPIOC_MODER, read_reg(GPIOC_MODER, ~(0x03 << 18))|(GPIO_MODER_OUTPUT << 18));
 
@@ -81,13 +72,11 @@ void init_pin(void)
   write_reg(GPIOA_MODER, read_reg(GPIOA_MODER, ~(0x03 << 0))|(GPIO_MODER_INPUT << 0));
 }
 
-void led_on(unsigned char pin_number)
-{
+void led_on(unsigned char pin_number){
 	write_reg(GPIOC_BSRR, 1u << pin_number);
 }
 
-void led_off(unsigned char pin_number)
-{
+void led_off(unsigned char pin_number){
 	write_reg(GPIOC_BSRR, 1u << (pin_number + 16u));
 }
 
