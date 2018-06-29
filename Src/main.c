2bfbf8634546fe080f42d4547f36fefbed2b214f
VINHPHAM\kilolt <vp3kilo@gmail.com>
2018-06-29 11:48:52 +0700

    UPDATE code chạy ngon GPIO2, input pin.

--- Src/main.c
+++ Src/main.c
@@ -65,12 +65,20 @@ void delay(unsigned int timeout)
 
 void enabled_clock(void)
 {
+  /* Enable clock PORT C*/
 	write_reg(RCC_AHBENR, read_reg(RCC_AHBENR, ~(1 << 19)) | (1 << 19));
+
+  /* Enable clock PORT A*/
+  write_reg(RCC_AHBENR, read_reg(RCC_AHBENR, ~(1 << 17)) | (1 << 17));
 }
 
 void init_pin(void)
 {
+  /* Init PC9 ~ OUTPUT*/
 	write_reg(GPIOC_MODER, read_reg(GPIOC_MODER, ~(0x03 << 18))|(GPIO_MODER_OUTPUT << 18));
+
+  /* Init PA0 ~ INPUT*/
+  write_reg(GPIOA_MODER, read_reg(GPIOA_MODER, ~(0x03 << 0))|(GPIO_MODER_INPUT << 0));
 }
 
 void led_on(unsigned char pin_number)
@@ -87,12 +95,24 @@ void main(void)
 {
 	enabled_clock();
 	init_pin();
+  unsigned state_idr_reg, led_stt = 0;
 	while(1)
 	{
-		led_on(LD3_PIN);
-		delay(0xff);
-		led_off(LD3_PIN);
-		delay(0xff);
+    /* Polling*/
+    state_idr_reg = read_reg(GPIOA_IDR, 1u << 0);
+    if (state_idr_reg == 1)
+    {
+      if (led_stt == 1)
+      {
+        led_off(LD3_PIN);
+        led_stt = 0;
+      }
+      else
+      {
+        led_on(LD3_PIN);
+        led_stt = 1;
+      }
+    }
 	}
 }
 
